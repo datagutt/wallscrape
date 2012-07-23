f4aff32785ad45bb5791340725d8eb63e74c14e8
datagutt <datagutt666@hotmail.com>
2012-07-23 20:11:35 +0200

    both scrapers at the same time

--- scrape.php
+++ scrape.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 error_reporting(-1);
 require 'Scraper.php';
 $scraper = new Scraper(array(
-	//'wallbase' => 'http://wallbase.cc/toplist'
+	'wallbase' => 'http://wallbase.cc/toplist'
 	'deviantart' => 'http://browse.deviantart.com/customization/wallpaper/'
 ));
 $scraper->run();
